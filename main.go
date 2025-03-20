315fcd4b9566108272260e6b1bdc26186c333792
StarByte <starcrusher202@gmail.com>
2025-03-20 15:11:08 +0100

    refactor: update module name and improve logging functionality

--- main.go
+++ main.go
@@ -3,15 +3,17 @@ package main
 import (
 	"context"
 	"fmt"
-	"io"
-	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 
+	"github.com/StarGames2025/Logger"
+
 	_ "github.com/mattn/go-sqlite3"
+
 	"go.mau.fi/whatsmeow"
 	"go.mau.fi/whatsmeow/store/sqlstore"
+	"go.mau.fi/whatsmeow/types"
 	"go.mau.fi/whatsmeow/types/events"
 	waLog "go.mau.fi/whatsmeow/util/log"
 
@@ -19,111 +21,89 @@ import (
 
 	"image"
 	"image/png"
-	"log"
 
 	"github.com/blacktop/go-termimg"
 	"github.com/nfnt/resize"
 )
 
-func DisplayImage(filePath string, width int, height int) string {
-	file, err := os.Open(filePath)
-	if err != nil {
-		log.Fatal(err)
+var (
+	logger       = Logger.NewLogger(Logger.DEBUG)
+	grupsList    []*types.GroupInfo
+	contactsList []struct {
+		JID     types.JID
+		Contact types.ContactInfo
 	}
-	defer file.Close()
-
-	img, _, err := image.Decode(file)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	resizedImg := resize.Resize(uint(width), uint(height), img, resize.Lanczos3)
-
-	tmpFile, err := os.Create("temp_resized.jpg")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer tmpFile.Close()
-
-	err = png.Encode(tmpFile, resizedImg)
-	if err != nil {
-		log.Fatal(err)
+	chatlist []struct {
+		JID  types.JID
+		Name string
 	}
 
-	ti, err := termimg.Open("temp_resized.jpg")
-	if err != nil {
-		log.Fatal(err)
+	exitCodes = map[string]int{
+		"ERROR":                -1,
+		"SUCCESS":              0,
+		"SHUTDOWN":             0,
+		"DB_INIT_ERROR":        10,
+		"DEVICE_STORE_ERROR":   11,
+		"CLIENT_CONNECT_ERROR": 12,
+		"QR_GENERATE_ERROR":    13,
+		"QR_OPEN_ERROR":        14,
+		"QR_DECODE_ERROR":      15,
+		"QR_RESIZE_ERROR":      16,
+		"QR_FILE_CREATE_ERROR": 17,
+		"QR_FILE_ENCODE_ERROR": 18,
+		"QR_RENDER_ERROR":      19,
+		"GROUP_FETCH_ERROR":    20,
+		"CONTACT_FETCH_ERROR":  21,
 	}
-	imgStr, err := ti.Render()
-	if err != nil {
-		log.Fatal(err)
-	}
+)
 
-	return imgStr
-}
-
-func GetImmage(imageString string, width int, height int) string {
+func GetQRCodeImage(imageString string, width int, height int) string {
+	logger.Info("Generating QR Code...")
 	err := qrcode.WriteFile(imageString, qrcode.Medium, 256, "qrcode.png")
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal("QR_GENERATE_ERROR", "Failed to generate QR Code:", err.Error())
 	}
 
 	file, err := os.Open("qrcode.png")
 	if err != nil {
-		log.Fatal(err)
+		logger.Error("QR_OPEN_ERROR", "Failed to open QR Code file:", err.Error())
 	}
 	defer file.Close()
 
 	img, _, err := image.Decode(file)
 	if err != nil {
-		log.Fatal(err)
+		logger.Error("QR_DECODE_ERROR", "Failed to decode QR Code image:", err.Error())
 	}
 
+	logger.Debug("Resizing QR Code image...")
 	resizedImg := resize.Resize(uint(width), uint(height), img, resize.Lanczos3)
+	if resizedImg.Bounds().Dx() == 0 || resizedImg.Bounds().Dy() == 0 {
+		logger.Error("QR_RESIZE_ERROR", "Resized QR Code image is invalid")
+	}
 
 	tmpFile, err := os.Create("temp_resized.png")
 	if err != nil {
-		log.Fatal(err)
+		logger.Error("Failed to create resized QR Code file:", err.Error())
 	}
 	defer tmpFile.Close()
 
 	err = png.Encode(tmpFile, resizedImg)
 	if err != nil {
-		log.Fatal(err)
+		logger.Error("Failed to encode resized QR Code:", err.Error())
 	}
+
+	logger.Info("QR Code generated successfully.")
 
 	ti, err := termimg.Open("temp_resized.png")
 	if err != nil {
-		log.Fatal(err)
+		logger.Error("Failed to render QR Code in terminal:", err.Error())
 	}
 	imgStr, err := ti.Render()
 	if err != nil {
-		log.Fatal(err)
+		logger.Error("Failed to get QR Code as string:", err.Error())
 	}
 
 	return imgStr
-}
-
-func downloadImage(url string, filename string) error {
-	resp, err := http.Get(url)
-	if err != nil {
-		return fmt.Errorf("Fehler beim Herunterladen des Bildes: %v", err)
-	}
-	defer resp.Body.Close()
-
-	out, err := os.Create(filename)
-	if err != nil {
-		return fmt.Errorf("Fehler beim Erstellen der Datei: %v", err)
-	}
-	defer out.Close()
-
-	_, err = io.Copy(out, resp.Body)
-	if err != nil {
-		return fmt.Errorf("Fehler beim Speichern des Bildes: %v", err)
-	}
-
-	fmt.Println("Bild erfolgreich gespeichert:", filename)
-	return nil
 }
 
 func eventHandler(evt interface{}) {
@@ -132,53 +112,110 @@ func eventHandler(evt interface{}) {
 		sender := v.Info.PushName
 		message := v.Message.GetConversation()
 
-		if message == "" {
-			downloadImage(v.Message.GetImageMessage().GetURL(), "received_image.jpg")
-			message = DisplayImage("received_image.jpg", 100, 100)
-		}
-
-		fmt.Printf("Received a message from %s: %s\n\r", sender, message)
+		logger.Info(fmt.Sprintf("Received a message from %s: %s\n\r", sender, message))
 	}
 }
 
 func main() {
-	dbLog := waLog.Stdout("Database", "", true)
+	logger = Logger.NewLogger(Logger.DEBUG)
+	logger.ExitCodes = exitCodes
+
+	logger.Info("Starting WhatsApp Client...")
+
+	dbLog := waLog.Stdout("Database", "ERROR", true)
 	container, err := sqlstore.New("sqlite3", "file:sqlstore.db?_foreign_keys=on", dbLog)
 	if err != nil {
-		panic(err)
+		logger.Error("DB_INIT_ERROR", "Failed to initialize database:", err.Error())
 	}
 
 	deviceStore, err := container.GetFirstDevice()
 	if err != nil {
-		panic(err)
+		logger.Error("DEVICE_STORE_ERROR", "Failed to get device store:", err.Error())
 	}
-	clientLog := waLog.Stdout("Client", "", true)
+
+	clientLog := waLog.Stdout("Client", "ERROR", true)
 	client := whatsmeow.NewClient(deviceStore, clientLog)
 	client.AddEventHandler(eventHandler)
 
 	if client.Store.ID == nil {
+		logger.Info("No session found, generating QR Code...")
 		qrChan, _ := client.GetQRChannel(context.Background())
 		err = client.Connect()
 		if err != nil {
-			panic(err)
+			logger.Error("CLIENT_CONNECT_ERROR", "Failed to connect client:", err.Error())
 		}
+
 		for evt := range qrChan {
 			if evt.Event == "code" {
-				fmt.Println("QR code:", GetImmage(evt.Code, 300, 300))
+				logger.Info("Displaying QR Code. Scan with WhatsApp.")
+				fmt.Println("QR code:", GetQRCodeImage(evt.Code, 300, 300))
 			} else {
-				fmt.Println("Login event:", evt.Event)
+				logger.Info("Login event received:", evt.Event)
 			}
 		}
 	} else {
+		logger.Info("Session found. Connecting...")
 		err = client.Connect()
 		if err != nil {
-			panic(err)
+			logger.Error("CLIENT_CONNECT_ERROR", "Failed to connect client:", err.Error())
+		}
+
+		logger.Info("Fetching joined groups...")
+		groups, err := client.GetJoinedGroups()
+		if err != nil {
+			logger.Error("GROUP_FETCH_ERROR", "Failed to fetch groups:", err.Error())
+		} else {
+			logger.Info(fmt.Sprintf("Found %d groups.", len(groups)))
+		}
+
+		for _, group := range groups {
+			logger.Debug(fmt.Sprintf("Group: %s - %s", group.JID, group.Name))
+			grupsList = append(grupsList, group)
+			chatlist = append(chatlist, struct {
+				JID  types.JID
+				Name string
+			}{
+				JID:  group.JID,
+				Name: group.Name,
+			})
+		}
+
+		logger.Info("Fetching contacts...")
+		contacts, err := client.Store.Contacts.GetAllContacts()
+		if err != nil {
+			logger.Error("CONTACT_FETCH_ERROR", "Failed to fetch contacts:", err.Error())
+		} else {
+			logger.Info(fmt.Sprintf("Found %d contacts.", len(contacts)))
+		}
+
+		for jid, contact := range contacts {
+			logger.Debug(fmt.Sprintf("Contact: %s - %s", jid, contact.PushName))
+			contactsList = append(contactsList, struct {
+				JID     types.JID
+				Contact types.ContactInfo
+			}{
+				JID:     jid,
+				Contact: contact,
+			})
+			chatlist = append(chatlist, struct {
+				JID  types.JID
+				Name string
+			}{
+				JID:  jid,
+				Name: contact.PushName,
+			})
 		}
 	}
 
+	// Signal-Handling
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	logger.Info("Client is running. Press Ctrl+C to stop.")
 	<-c
 
+	logger.Info("Shutting down...")
 	client.Disconnect()
+	logger.Info("Disconnected. ")
+	logger.Info("Exiting.")
+	os.Exit(exitCodes["SHUTDOWN"])
 }
